7200d841b871a32506a1fd8b90d450ae59c76201
Patrik Johansson <2812422+improwise@users.noreply.github.com>
2024-09-03 01:49:51 +0200

    Fixed broken images

--- MoviePortal/Models/Movie.cs
+++ MoviePortal/Models/Movie.cs
@@ -1,9 +1,13 @@
-﻿namespace MoviePortal.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace MoviePortal.Models;
 
 public class Movie
 {
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
-    public string PosterPath { get; set; } = string.Empty;
-    public List<int> GenreIds { get; set; } = new();
+
+    [JsonPropertyName("poster_path")] public string? PosterPath { get; set; }
+
+    [JsonPropertyName("genre_ids")] public List<int> GenreIds { get; set; } = new();
 }
